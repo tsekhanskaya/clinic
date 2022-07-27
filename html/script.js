89059c5c73d93fdf97a03e99f065ee3a4ce05e3a
tsekhanskaya <anutta2222@gmail.com>
2022-07-27 15:37:03 +0300

    Added showElements function, fixed files that use this function

--- html/script.js
+++ html/script.js
@@ -1,80 +1,69 @@
-function getelements(elements) {
-    $.ajax('http://localhost:3000/' + elements,
-        {
-            success: function (data) {
-                if (elements === 'employees') {
-                    let elements = ""
-                    data.forEach(element => {
-                    {
-                        elements += "Идентификатор сотрудника: " + element.id +
-                                    "; ФИО сотрудника: " + element.fullname +
-                                    "; Идентификатор должности: "+element.position_id+
-                                    "; Идентификатор специализации: "+ element.specialization_id".\n"
-                    }
-                  })
-                  alert(elements)
-                }
-                if (elements === 'specializations') {
-                    let elements = ""
-                    data.forEach(element => {
-                        {
-                            elements += "Идентификатор специализации: " + element.id +
-                            "; Наименование специализации: " + element.specialty + ".\n"
-                        }
-                    })
-                    alert(elements)
-                }
-                if (elements === 'positions') {
-                    let elements = ""
-                    data.forEach(element => {
-                        {
-                            elements += "Идентификатор должности: " + element.id +
-                            "; Наименование должности: " + element.post + ".\n"
-                        }
-                    })
-                    alert(elements)
-                }
-                if (elements === 'patients') {
-                    let elements = ""
-                    data.forEach(element => 
-                    {
-                        elements += "Идентификатор пациента: " + element.id +
-                                    "; Фамилия: " + element.last_name +
-                                    "; Имя: " + element.first_name+
-                                    "; Отчество: "+element.patronymic+
-                                    "; Дата рождения: "+element.birthdate+
-                                    "; Телефон: "+element.phone".\n"
-                    }
-                    )
-                  alert(elements)
-                }
-                if (elements === 'disease') {
-                    let elements = ""
-                    data.forEach(element => {
-                        {
-                            elements += "Идентификатор диагноза: " + element.id +
-                            "; Код диагноза: " + element.code +
-                            "; Название диагноза: " + element.disease_name + ".\n"
-                        }
-                    })
-                    alert(elements)
-                }
-                if (elements === 'medical_reports') {
-                    let elements = ""
-                    data.forEach(element => {
-                    {
-                        elements += "Идентификатор медицинского отчета: " + element.id +
-                                    "; Дата записи: " + element.data_examination +
-                                    "; Наличие больчного: " + element.sick_leave+
-                                    "; Назначенные медицинские препараты: "+element.prescribing+
-                                    "; Рекомендация использования прапаратов: "+element.recommendation+
-                                    "; Идентификатор пациента: "+element.patient_id+
-                                    "; Идентификатор сотрудника: " + element.employee_id +
-                                    "; Идентификатор диагноза: "+element.disease_id".\n"
-                    }
-                  })
-                  alert(elements)
-                }
-            }
-        })
+function showElements(elements) {
+  $.ajax('http://localhost:3000/' + elements,
+      {
+        success: function (data) {
+          if (elements === 'employees') {
+            let elements = "";
+              data.forEach(element => {
+                elements += "Идентификатор сотрудника: " + element.id +
+                            ";\nФИО сотрудника: " + element.fullname +
+                            ";\nИдентификатор должности: " + element.position_id +
+                            ";\nИдентификатор специализации: " + element.specialization_id + ".\n\n";
+               })
+            return alert(elements);
+          }
+          if (elements === 'specializations') {
+            let elements = "";
+              data.forEach(element => {
+                elements += "Идентификатор специализации: " + element.id +
+                            ";\nНаименование специализации: " + element.specialty + ".\n\n";
+              })
+           return alert(elements);
+          }
+          if (elements === 'positions') {
+            let elements = "";
+            data.forEach(element => {
+              elements += "Идентификатор должности: " + element.id +
+                          ";\nНаименование должности: " + element.post + ".\n\n"
+            })
+            return alert(elements);
+          }
+          if (elements === 'patients') {
+            let elements = "";
+            data.forEach(element => {
+              elements += "Идентификатор пациента: " + element.id +
+                          ";\nФамилия: " + element.last_name +
+                          ";\nИмя: " + element.first_name +
+                          ";\nОтчество: " + element.patronymic +
+                          ";\nДата рождения: " + element.birthdate +
+                          ";\nТелефон: " + element.phone + ".\n\n";
+            })
+            return alert(elements);
+          }
+          if (elements === 'diseases') {
+              let elements = "";
+              data.forEach(element => {
+                elements += "Идентификатор диагноза: " + element.id +
+                            ";\nКод диагноза: " + element.code +
+                            ";\nНазвание диагноза: " + element.disease_name + ".\n\n";
+              })
+              return alert(elements);
+          }
+          if (elements === 'medical_reports') {
+              let elements = "";
+              data.forEach(element => {
+                elements += "Идентификатор медицинского отчета: " + element.id +
+                            ";\nДата записи: " + element.data_examination +
+                            ";\nНаличие больчного: " + element.sick_leave +
+                            ";\nНазначенные медицинские препараты: " + element.prescribing +
+                            ";\nРекомендация использования препаратов: " + element.recommendation +
+                            ";\nИдентификатор пациента: " + element.patient_id +
+                            ";\nИдентификатор сотрудника: " + element.employee_id +
+                            ";\nИдентификатор диагноза: " + element.disease_id + ".\n\n";
+              })
+              return alert(elements);
+          }
+        }
+      }
+    )
 }
